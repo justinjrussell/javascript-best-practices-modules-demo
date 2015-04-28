0dac94607bb77997b5842ecda0d4a8ad367962e5
justinjrussell <justinrussell@outlook.com>
2015-04-27 23:13:03 -0700

    Update trimmer.test.js

--- 4 CommonJS/test/trimmer.test.js
+++ 4 CommonJS/test/trimmer.test.js
@@ -9,9 +9,11 @@ describe("Trimmer Module", function () {
         assert.equal(trimmer.Trim(" This is a test "), expected, "Passed: Trim");
         assert.equal(trimmer.TrimStart(" This is a test"), expected, "Passed: TrimStart");
         assert.equal(trimmer.TrimEnd("This is a test "), expected, "Passed: TrimEnd");
+        assert.doesNotThrow(function () { trimmer.Trim(""); }, "Passed: Does not throw error");
+        assert.doesNotThrow(function () { trimmer.TrimStart(""); }, "Passed: Does not throw error");
+        assert.doesNotThrow(function () { trimmer.TrimEnd(""); }, "Passed: Does not throw error");
     });
     it("Trim Should Throw Error if not string", function () {
-        assert.doesNotThrow(function () { trimmer.Trim(""); }, "Passed: Does not throw error");
         assert.Throw(function () { trimmer.Trim(0); }, Error, "Invalid Argument", "Passed: Throws Error");
         assert.Throw(function () { trimmer.Trim(true); }, Error, "Invalid Argument", "Passed: Throws Error");
         assert.Throw(function () { trimmer.Trim({}); }, Error, "Invalid Argument", "Passed: Throws Error");
@@ -19,7 +21,6 @@ describe("Trimmer Module", function () {
         assert.Throw(function () { trimmer.Trim(function () { }); }, Error, "Invalid Argument", "Passed: Throws Error");
     });
     it("TrimStart Should Throw Error if not string", function () {
-        assert.doesNotThrow(function () { trimmer.TrimStart(""); }, "Passed: Does not throw error");
         assert.Throw(function () { trimmer.TrimStart(0); }, Error, "Invalid Argument", "Passed: Throws Error");
         assert.Throw(function () { trimmer.TrimStart(true); }, Error, "Invalid Argument", "Passed: Throws Error");
         assert.Throw(function () { trimmer.TrimStart({}); }, Error, "Invalid Argument", "Passed: Throws Error");
@@ -27,7 +28,6 @@ describe("Trimmer Module", function () {
         assert.Throw(function () { trimmer.TrimStart(function () { }); }, Error, "Invalid Argument", "Passed: Throws Error");
     });
     it("TrimEnd Should Throw Error if not string", function () {
-        assert.doesNotThrow(function () { trimmer.Trim(""); }, "Passed: Does not throw error");
         assert.Throw(function () { trimmer.TrimEnd(0); }, Error, "Invalid Argument", "Passed: Throws Error");
         assert.Throw(function () { trimmer.TrimEnd(true); }, Error, "Invalid Argument", "Passed: Throws Error");
         assert.Throw(function () { trimmer.TrimEnd({}); }, Error, "Invalid Argument", "Passed: Throws Error");
